92d0fc832baee3d2809363c4cca98400b250f045
Hongxu Wang <hongxuwa@Hongxus-MacBook-Pro.local>
2024-10-02 13:44:16 -0700

    update test document

--- tests/testthat/test-BaHZING_Model.R
+++ tests/testthat/test-BaHZING_Model.R
@@ -7,20 +7,33 @@ test_that("test BaHZING_Model", {
 
   ### Perform Bayesian hierarchical zero-inflated negative binomial regression with g-computation
   ### Specify a mixture of exposures
-  x <- c("soft_drinks_dietnum","diet_soft_drinks_dietnum","fruit_juice_dietnum","water_dietnum",
-         "alcohol_dietnum","yogurt_dietnum","dairy_dietnum","probiotic_dietnum","fruits_no_juice_dietnum",
-         "vegetables_dietnum","beans_soy_dietnum","whole_grains_dietnum","starch_dietnum","eggs_dietnum",
-         "processed_meat_dietnum","red_meat_dietnum","white_meat_dietnum","shellfish_dietnum","fish_dietnum",
-         "sweets_dietnum")
+  x <- c("soft_drinks_dietnum","diet_soft_drinks_dietnum"
+         # "fruit_juice_dietnum","water_dietnum",
+         # "alcohol_dietnum","yogurt_dietnum","dairy_dietnum","probiotic_dietnum","fruits_no_juice_dietnum",
+         # "vegetables_dietnum","beans_soy_dietnum","whole_grains_dietnum","starch_dietnum"
+         # ,"eggs_dietnum",
+         # "processed_meat_dietnum","red_meat_dietnum","white_meat_dietnum","shellfish_dietnum","fish_dietnum",
+         # "sweets_dietnum"
+         )
 
   ### Specify a set of covariates
-  covar <- c("consent_age","sex",paste0("race",0:3),paste0("educ",0:7))
+  # covar <- c("consent_age","sex",paste0("race",0:3),paste0("educ",0:7))
+
+  covar <- c("consent_age")
 
   ###Set exposure standaridization to standard_normal or quantiles
   exposure_standardization = "standard_normal"
 
+  n.chains = 1
+  n.adapt = 60
+
   ### Perform BaH-ZING
-  results <- BaHZING_Model(formatted_data,covar,x,exposure_standardization)
+  results <- BaHZING_Model(formatted_data,
+                           covar,
+                           x,
+                           exposure_standardization,
+                           n.chains,
+                           n.adapt)
   # Test format data
   testthat::expect_equal(object = ncol(results), expected = 7)
 })
--- tests/testthat/test-Format_BaHZING.R
+++ tests/testthat/test-Format_BaHZING.R
@@ -1,25 +1,15 @@
 test_that("test Format_BaHZING", {
-  PS <- iHMP
-  formatted_data <- Format_BaHZING(PS)
+  # Load data
+  data("iHMP_Reduced")
+  # Format microbiome data
+  formatted_data <- Format_BaHZING(iHMP_Reduced)
   # Test format data
   testthat::expect_equal(object = length(formatted_data), expected = 6)
   testthat::expect_true(is.list(formatted_data))
   testthat::expect_true("Table" %in% names(formatted_data))
 })
 
-# test_that("taxa_are_rows transposes correctly",{
-#   # OTU table with taxa are in rows
-#   PS <- iHMP
-#   otu_matrix <- iHMP@otu_table@.Data %>% t()
-#   OTU <- otu_table(otu_matrix, taxa_are_rows = TRUE)
-#   PS@otu_table <- OTU
-#
-#   formatted_data <- Format_BaHZING(PS)
-#   # Check that taxa are in rows
-#   testthat::expect_equal(object = length(formatted_data), expected = 6)
-#   testthat::expect_true(is.list(formatted_data))
-#   testthat::expect_true("Table" %in% names(formatted_data))
-# })
+
 
 test_that("Error thrown when taxonomic levels are less than 2", {
 
@@ -31,17 +21,6 @@ test_that("Error thrown when taxonomic levels are less than 2", {
   testthat::expect_error(Format_BaHZING(PS), "Need > 1 taxonomic level")
   })
 
-# test_that("Error thrown when missing Kingdom-level information", {
-#   PS <- iHMP
-#
-#   # Set one Kingdom level to NA
-#   tax_table(PS)[1, "Kingdom"] <- NA
-#
-#   # Expect an error to be thrown due to missing Kingdom-level information
-#   testthat::expect_error(Format_BaHZING(PS),
-#                          "Missing Kindom-level information for an ASV/OTU. Remove unidentified bacteria and rerun.")
-#
-# })
 
 test_that("If species level not present, create species column", {
   PS <- iHMP
